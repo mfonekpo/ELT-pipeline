d43234a3f7ad8ac5b7d6db42c779fe1ba5670f7f
mfonekpo <mfonekpo34@gmail.com>
2024-03-26 17:04:42 +0100

    Exception handling

--- ELT_pipeline/data_pipeline_mod.py
+++ ELT_pipeline/data_pipeline_mod.py
@@ -32,10 +32,14 @@ urls = []
 def fetch_page(url, page):
     logger = get_run_logger()
     logger.info("Fetching Site Data")
-    response = requests.get(f"{url}{page}")
-    response.raise_for_status()
-    soup = bs(response.text, "html.parser")
-    return soup
+    try:
+        response = requests.get(f"{url}{page}")
+        response.raise_for_status()
+        soup = bs(response.text, "html.parser")
+        return soup
+    except Exception as e:
+        logger.error(e)
+        raise
 
 @task(
     name="parse_names",
@@ -47,9 +51,13 @@ def fetch_page(url, page):
 def parse_names(soup, name_tag:str):
     logger = get_run_logger()
     logger.info("parsing phase started >> Parsing Names")
-    for name in soup.select(name_tag):
-        names.append(name.text.strip())
-    return names
+    try:
+        for name in soup.select(name_tag):
+            names.append(name.text.strip())
+        return names
+    except Exception as e:
+        logger.error(e)
+        raise
 
 @task(
     name="parse_locations",
@@ -59,12 +67,16 @@ def parse_names(soup, name_tag:str):
     cache_expiration=timedelta(days=1)
 )
 def parse_locations(soup, location_tag:str):
-    tag_name, tag_attrs = location_tag
     logger = get_run_logger()
     logger.info("Parsing Locations from the website")
-    for loc in soup.find_all(tag_name, **tag_attrs):
-        locations.append(loc.find_all("p")[1].text.strip())
-    return locations
+    try:
+        tag_name, tag_attrs = location_tag
+        for loc in soup.find_all(tag_name, **tag_attrs):
+            locations.append(loc.find_all("p")[1].text.strip())
+        return locations
+    except Exception as e:
+        logger.error(e)
+        raise
 
 @task(
     name="parse_categories",
@@ -76,10 +88,14 @@ def parse_locations(soup, location_tag:str):
 def parse_categories(soup, category_tag:str):
     logger = get_run_logger()
     logger.info("Parsing Categories from the website")
-    tag_name, tag_attrs = category_tag
-    for cat in soup.find_all(tag_name, **tag_attrs):
-        categories.append(cat.find_all("p")[0].text.strip())
-    return categories
+    try:
+        tag_name, tag_attrs = category_tag
+        for cat in soup.find_all(tag_name, **tag_attrs):
+            categories.append(cat.find_all("p")[0].text.strip())
+        return categories
+    except Exception as e:
+        logger.error(e)
+        raise
 
 @task(
     name="parse_descriptions",
@@ -91,10 +107,14 @@ def parse_categories(soup, category_tag:str):
 def parse_descriptions(soup, description_tag:str):
     logger = get_run_logger()
     logger.info("Parsing Descriptions from the website")
-    tag_name, tag_attrs = description_tag
-    for desc in soup.find_all(tag_name, **tag_attrs):
-        descriptions.append(desc.text.strip().replace("\n", "").replace("\r", ""))
-    return descriptions
+    try:
+        tag_name, tag_attrs = description_tag
+        for desc in soup.find_all(tag_name, **tag_attrs):
+            descriptions.append(desc.text.strip().replace("\n", "").replace("\r", ""))
+        return descriptions
+    except Exception as e:
+        logger.error(e)
+        raise
 
 @task(
     name="parse_likes",
@@ -106,10 +126,14 @@ def parse_descriptions(soup, description_tag:str):
 def parse_likes(soup, like_tag:str):
     logger = get_run_logger()
     logger.info("Parsing Likes from the website")
-    tag_name, tag_attrs = like_tag
-    for like in soup.find_all(tag_name, **tag_attrs):
-        likes.append(like.find_all("span")[1].text.strip())
-    return likes
+    try:
+        tag_name, tag_attrs = like_tag
+        for like in soup.find_all(tag_name, **tag_attrs):
+            likes.append(like.find_all("span")[1].text.strip())
+        return likes
+    except Exception as e:
+        logger.error(e)
+        raise
 
 @task(
     name="parse_urls",
@@ -121,10 +145,14 @@ def parse_likes(soup, like_tag:str):
 def parse_urls(soup, url_tag:str):
     logger = get_run_logger()
     logger.info("Parsing urls from the website")
-    tag_name, tag_attrs = url_tag
-    for url in soup.find_all(tag_name, **tag_attrs):
-        urls.append(base_url+url.find_all("a")[0]["href"])
-    return urls
+    try:
+        tag_name, tag_attrs = url_tag
+        for url in soup.find_all(tag_name, **tag_attrs):
+            urls.append(base_url+url.find_all("a")[0]["href"])
+        return urls
+    except Exception as e:
+        logger.error(e)
+        urls.append(None)
 
 
 @task(
@@ -137,16 +165,20 @@ def parse_urls(soup, url_tag:str):
 def add_data_to_dict(names, locations, categories, descriptions, likes, urls):
     logger = get_run_logger()
     logger.info(" performing data aggregate")
-    dict_data = {
-        "name_of_sight": names,
-        "location": locations,
-        "category": categories,
-        "description": descriptions,
-        "likes_count": likes,
-        "location_url": urls
-    }
-    logger.info("Data aggregated successfully")
-    return dict_data
+    try:
+        dict_data = {
+            "name_of_sight": names,
+            "location": locations,
+            "category": categories,
+            "description": descriptions,
+            "likes_count": likes,
+            "location_url": urls
+        }
+        logger.info("Data aggregated successfully")
+        return dict_data
+    except Exception as e:
+        logger.error(e)
+        raise
 
 
 @task(
@@ -154,23 +186,30 @@ def add_data_to_dict(names, locations, categories, descriptions, likes, urls):
     log_prints=True,
     retries=3,
     # cache_key_fn=task_input_hash,
-    cache_expiration=timedelta(days=1)
+    # cache_expiration=timedelta(days=1)
 )
 def load_to_db(dict_data):
     logger = get_run_logger()
-    logger.info("Loading data to database")
-    conn = sqlite3.connect("data.db")
-    df = pd.DataFrame(dict_data)
-    df.to_sql("data", conn, if_exists="replace", index = False)
-    logger.info("Data loaded successfully")
+    try:
+        logger.info("Loading data to database")
+        conn = sqlite3.connect("../datafiles/data.db")
+        df = pd.DataFrame(dict_data)
+        df.to_sql("data", conn, if_exists="replace", index = False)
+        conn.close()
+        logger.info("Data loaded successfully")
+    except Exception as e:
+        # logger.error(f"Failed to load data to database {e}")
+        logger.error(e)
+        raise
+
 
 
 @task(
     name="load_to_csv",
     log_prints=True,
-    retries=3,
-    cache_key_fn=task_input_hash,
-    cache_expiration=timedelta(days=1)
+    retries=3
+    # cache_key_fn=task_input_hash,
+    # cache_expiration=timedelta(days=1)
 )
 def load_to_csv(dict_data):
     logger = get_run_logger()
@@ -182,8 +221,9 @@ def load_to_csv(dict_data):
 
 
 
-@flow(name = "Interesting places extraction pipeline")
+@flow(name = "Interesting places extraction pipeline", log_prints=True)
 def main_flow(url = "https://hotels.ng/places/nigeria-1/", pages = 3):
+    logger = get_run_logger()
     name_tag = "a > h2"
     location_tag = ("div", {"class":"category-de01"})
     category_tag = "div", {"class":"category-de01"}
@@ -194,18 +234,21 @@ def main_flow(url = "https://hotels.ng/places/nigeria-1/", pages = 3):
 
     for page in range(1, pages+1):
         print(f"Extracting data from page{page}")
+        # try:
         soup = fetch_page(url, page)
         names = parse_names(soup, name_tag)
         locations = parse_locations(soup, location_tag)
         categories = parse_categories(soup, category_tag)
         descriptions = parse_descriptions(soup, description_tag)
         likes = parse_likes(soup, like_tag)
-        urls = parse_urls(soup, url_tag)
-        
-    dict_data = add_data_to_dict(names, locations, categories, descriptions, likes, urls)
-    load_to_db(dict_data)
-    load_to_csv(dict_data)
-    print(dict_data)
+        urls = parse_urls(soup, url_tag, return_state=True)
+        dict_data = add_data_to_dict(names, locations, categories, descriptions, likes, urls)
+        load_to_db(dict_data)
+        load_to_csv(dict_data)
+        # except Exception as e:
+        #     logger.error(e)
+
+
     return dict_data
 
 
